b253d9c5d1f88ee970848a4c70ca4b3f812107bf
wixgung <wixgung@gmail.com>
2026-08-12 08:23:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // KONFIGURASI PORTAL — edit HANYA baris di bawah ini
 // Tempel URL Web App Apps Script kamu (lihat PANDUAN-SPREADSHEET.md)
 // ============================================================
-const CONTROL_URL = "https://script.google.com/macros/s/GANTI_DENGAN_ID_DEPLOYMENT/exec";
+const CONTROL_URL = "https://script.google.com/macros/s/AKfycbwn4FJDvzbXrydGRgBgXES3pU1-kDVSkGKGmYbG23gv719hLzCd07psSL5nDqOSJ3Tc/exec";
